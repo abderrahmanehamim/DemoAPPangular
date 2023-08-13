a82fe9d9ec35156cfdd6b0916a04188d8149680e
abdouhamim <abderahmane.hamim@gmail.com>
2023-08-13 03:54:33 +0200

    add routes.ts add router outlet and router link make standalone true in component (app.comp.Ts)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  template: `
-                            <main>
-                              <header class="brand-name">
-                                <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-                              </header>
-                              <section class="content">
-                                <app-home></app-home>
-                              </section>
-                            </main>
-`,
-  styleUrls: ['./app.component.css'],
+  imports: [
+    HomeComponent,
+    RouterModule,
 
+  ],
+  template: `
+        <main>
+          <a [routerLink]="['/']">
+            <header class="brand-name">
+              <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+            </header>
+          </a>
+          <section class="content">
+            <router-outlet></router-outlet>
+          </section>
+        </main>
+        `,
+  styleUrls: ['./app.component.css'],
+  standalone: true,
 })
 export class AppComponent {
   title = 'Homes';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  }
+).catch(err => console.error(err));
